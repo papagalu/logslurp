5e57714f2d0fae0d90467135cdb578da57c4ba6c
papagalu <dmititelu@cloudbasesolutions.com>
2018-07-23 19:53:08 +0300

    Small changes to logslurp
        - parameterized output folder

--- logslurp.ps1
+++ logslurp.ps1
@@ -1,33 +1,39 @@
 #!/usr/bin/pwsh
-Param(
-  [string]$win_user,
-  [string]$win_pass
+param(
+    [String] $WinUser,
+    [String] $WinPass,
+    [String] $OutputFolder
 )
 
-$lockedFiles = "kubelet.err.log", "kubelet.log", "kubeproxy.log", "kubeproxy.err.log"
-if (($PSBoundParameters.ContainsKey("win_user")) -and ($PSBoundParameters.ContainsKey("win_pass")))
-{ 
-   $passwd = ConvertTo-SecureString $win_pass -AsPlainText -Force
-   $cred = New-Object System.Management.Automation.PSCredential ($win_user, $passwd) 
+function Main {
+    $lockedFiles = "kubelet.err.log", "kubelet.log", "kubeproxy.log", "kubeproxy.err.log"
+
+    $passwd = ConvertTo-SecureString $WinPass -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential ($WinUser, $passwd) 
+
+    $nodes = kubectl get node -o json | ConvertFrom-Json
+
+    foreach ($node in $nodes.items) {
+        if ($node.metadata.labels.'beta.kubernetes.io/os' -eq 'windows') {
+            Add-Member -InputObject $node -MemberType NoteProperty -Name "PSSession" `
+                -Value (New-PSSession -ComputerName $node.status.nodeInfo.machineID `
+                -Credential $cred -UseSSL -Authentication basic)
+
+            Write-Host "Connected to " + $node.status.nodeInfo.machineID
+            # Write-Host Logs:
+            $zipName = "$($node.status.nodeInfo.machineID)-$(get-date -format 'yyyyMMdd-hhmmss')_logs.zip"
+            $remoteZipPath = Invoke-Command -Session $node.PSSession { 
+                $paths = Get-ChildItem C:\k\*.log -Exclude $using:lockedFiles
+                $paths += $using:lockedFiles | Foreach-Object { Copy-Item "c:\k\$node" . -Passthru }
+                # docker ps
+                docker ps  > "docker_ps.txt"
+                $paths += "docker_ps.txt"
+                Compress-Archive -Path $paths -DestinationPath $using:zipName
+                Get-ChildItem $using:zipName
+            } 
+            Copy-Item -FromSession $node.PSSession $remoteZipPath -Destination $OutputFolder
+        }
+    }
 }
-else {
-   $cred = Get-Credential -Message "Please enter an admin username & password to connect to the Windows nodes"
-}
-$nodes = ./kubectl get node -o json | ConvertFrom-Json
-$nodes.items | Where-Object { $_.metadata.labels.'beta.kubernetes.io/os' -eq 'windows' } | foreach-object {
-  Add-Member -InputObject $_ -MemberType NoteProperty -Name "pssession" -Value (New-PSSession -ComputerName $_.status.nodeInfo.machineID -Credential $cred -UseSSL -Authentication basic)
-  Write-Host Connected to $_.status.nodeInfo.machineID
-  # Write-Host Logs:
-  $zipName = "$($_.status.nodeInfo.machineID)-$(get-date -format 'yyyyMMdd-hhmmss')_logs.zip"
-  $remoteZipPath = Invoke-Command -Session $_.pssession { 
-    $paths = get-childitem c:\k\*.log -Exclude $using:lockedFiles
-    $paths += $using:lockedFiles | Foreach-Object { Copy-Item "c:\k\$_" . -Passthru }
-    # docker ps
-    docker ps  > "docker_ps.txt"
-    $paths += "docker_ps.txt"
-    Compress-Archive -Path $paths -DestinationPath $using:zipName
-    Get-ChildItem $using:zipName
-  } 
-  Copy-Item -FromSession $_.pssession $remoteZipPath -Destination out/
-  
-}
+
+Main
